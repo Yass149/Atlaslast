5ccfa548a43c3bfb43700651a4f8892e23c61221
FahdAzouz <104470633+FahdAzouz@users.noreply.github.com>
2023-05-20 23:14:36 +0100

    vrs2

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,8 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
 
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
-const supabaseKey = process.env.REACT_APP_ANON_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-export default supabase
+// Create a single supabase client for interacting with your database
+export const supabase = createClient(
+    process.env.REACT_APP_SUPABASE_URL, 
+    process.env.REACT_APP_ANON_KEY
+);
