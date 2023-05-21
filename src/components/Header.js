fced3d734f8c267f2d2ff6594d5812f254245f05
Yass149 <48418035+Yass149@users.noreply.github.com>
2023-05-21 01:35:32 +0100

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,42 @@
 import React from 'react';
-
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Logo from '../assets/img/Logo1.png';
 
 const Header = () => {
+  const handleLogin = () => {
+    // Add your login functionality here
+    console.log('Login button clicked');
+    // Perform login actions
+  };
+console.log(handleLogin);
+
+  const handleSignUp = () => {
+    // Add your sign up functionality here
+    console.log('Sign up button clicked');
+    // Perform sign up actions
+  };
+
   return (
-  <header className='px-24 mb-12 border-b'>
-    <div className='mx-auto w-100 h-14 left-0 flex justify-between items-center'>
-      {/* logo */}
-      <Link to='/'>
-        <img src={Logo} alt='' />
-      </Link>
-      {/* bUTTONS */}
-      <div className='flex items-center gap-6'>
-      <Link className='hover.text-Blue-900'>Log in</Link>
-      <Link className='bg-blue-700 hover:bg-Blue-800 text-white px-4 py-3 rounded-lg transition'>Sign up</Link>
+    <header className='px-24 mb-12 border-b'>
+      <div className='mx-auto w-100 h-14 left-0 flex justify-between items-center'>
+        {/* logo */}
+        <Link to='/'>
+          <img src={Logo} alt='' />
+        </Link>
+        {/* buttons */}
+        <div className='flex items-center gap-6'>
+          <Link onClick={handleLogin} className='hover:text-Blue-900'>
+            Log in
+          </Link>
+          <Link
+            onClick={handleSignUp}
+            className='bg-blue-700 hover:bg-Blue-800 text-white px-4 py-3 rounded-lg transition'
+          >
+            Sign up
+          </Link>
+        </div>
       </div>
-    </div>
-  </header> 
+    </header>
   );
 };
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,52 +1,51 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
-function LoginPage() {
+const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const history = useHistory();
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
+  const handleLogin = (e) => {
     e.preventDefault();
+    // Add your login functionality here
+    console.log('Login button clicked');
+    console.log('Email:', email);
+    console.log('Password:', password);
+    // Perform login actions
 
-    // Basic email validation
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-      alert('Please enter a valid email address.');
-      return;
-    }
-
-    // Password validation (can be customized as needed)
-    if (password.length < 6) {
-      alert('Password must be at least 6 characters long.');
-      return;
-    }
-
-    // Login successful
-    alert('Login successful!');
-    // Perform additional actions like sending login request to a server
+    // Redirect to the home page or any other desired page after successful login
+    history.push('/');
   };
 
   return (
-    <div>
-      <h2>Login Page</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" value={email} onChange={handleEmailChange} required /><br /><br />
-
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" value={password} onChange={handlePasswordChange} required /><br /><br />
-
-        <input type="submit" value="Login" />
+    <div className="container">
+      <h1>Login Page</h1>
+      <form>
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button onClick={handleLogin}>Log in</button>
       </form>
     </div>
   );
-}
+};
 
-export default LoginPage;
+export default Login;
